d2ec3150470515c85f27a1e461e25b56046d5ff9
Geroxus <goldto@web.de>
2023-10-15 12:58:16 +0200

    pylint

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
+"""main"""
+import sys
 from typing import Callable
-
-import requests as requests
-
 from sys import argv
-from courseInformationApplicationService import printEmailsForAllParticipantsInCourse, checkAvailabilityOfCourses
-from loginService import login
+
+import requests
+
+from course_information_application_service import (print_emails_for_all_participants_in_course,
+                                                    check_availability_of_courses)
+from login_service import login
 
 URL_LOGIN = "https://e-learning.tuhh.de/studip/index.php?again=yes"
 
 
-def executeInSession(funtion: Callable, *args: str):
+def execute_in_session(funtion: Callable, *args: str):
+    """wraps the callable in a with session body and performs login
+    :param funtion:
+    :param args:
+    :return:
+    """
     with requests.Session() as session:
         if login(session, URL_LOGIN):
             funtion(session, args)
-            exit(0)
-    exit(1101)
+            sys.exit(0)
+    sys.exit(1101)
 
 
 if __name__ == '__main__':
 
+    # TODO move cli args interpreter to individual service <- maybe actually leave it here though
+    #                                                                 as it is the main controller
     if len(argv) == 1:
         print("you have to enter something to do")
-        exit(1001)
+        sys.exit(1001)
     else:
         if argv[1] == "courseParticipantMails" and len(argv) == 3:
-            courseName = argv[2]
-            executeInSession(printEmailsForAllParticipantsInCourse, argv[2])
+            execute_in_session(print_emails_for_all_participants_in_course, argv[2])
         elif argv[1] == "courseAvailabilityCheck" and len(argv) == 3:
-            executeInSession(checkAvailabilityOfCourses, *argv[2].split(","))
+            execute_in_session(check_availability_of_courses, *argv[2].split(","))
         else:
             print("this is not a valid command")
-            exit(1011)
+            sys.exit(1011)
         for arg in argv:
             print(arg)
-
-
