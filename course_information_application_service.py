24cdc14587ed8ea71196ab66920c73b41fa086d5
Geroxus <goldto@web.de>
2024-03-05 01:39:41 +0100

    pylint

--- course_information_application_service.py
+++ course_information_application_service.py
@@ -48,8 +48,9 @@ def check_availability_of_courses(session: requests.Session, args: tuple[str]) -
             'erlaubt': 0,
             'unbekannt': 0,
         }
-        url = (f"https://e-learning.tuhh.de/studip/dispatch.php/globalsearch/find/100?search={course}&filters=%7B"
-               f"%22category%22%3A%22show_all_categories%22%2C%22semester%22%3A%22%22%7D")
+        url = (f"https://e-learning.tuhh.de/studip/dispatch.php/globalsearch/find/100?search="
+               f"{course}&filters=%7B%22category%22%3A%22show_all_"
+               f"categories%22%2C%22semester%22%3A%22%22%7D")
         search_course_response = session.get(url)
         content = json.loads(search_course_response.text)["GlobalSearchCourses"]["content"]
         print("nr of courses found:", len(content))
@@ -57,13 +58,14 @@ def check_availability_of_courses(session: requests.Session, args: tuple[str]) -
             if "23/24" in item["date"]:
                 # hier soll vor allem eine unterscheidung zu aktuellem semester stattfinden!
                 pass
-            url_course = f"https://e-learning.tuhh.de/studip/dispatch.php/course/details/index/{item['id']}"
+            url_course = (f"https://e-learning.tuhh.de/studip/dispatch.php/course/details/index/"
+                          f"{item['id']}")
             text = session.get(url_course).text
-            if any(["Anmeldung gesperrt" in x for x in text.splitlines()]):
+            if any("Anmeldung gesperrt" in x for x in text.splitlines()):
                 zugang['gesperrt'] += 1
-            elif any(["Zeitgesteuerte Anmeldung" in x for x in text.splitlines()]):
+            elif any("Zeitgesteuerte Anmeldung" in x for x in text.splitlines()):
                 zugang['zeit'] += 1
-            elif any(["Anmeldung mit Passwort" in x for x in text.splitlines()]):
+            elif any("Anmeldung mit Passwort" in x for x in text.splitlines()):
                 zugang['password'] += 1
             else:
                 attempt_apply(session, item, zugang)
@@ -71,9 +73,18 @@ def check_availability_of_courses(session: requests.Session, args: tuple[str]) -
 
 
 def attempt_apply(session: requests.Session, item: dict, zugang: dict) -> None:
-    url_apply = f"https://e-learning.tuhh.de/studip/dispatch.php/course/enrolment/apply/{item['id']}"
+    """
+    attempts to apply for a course in the item parameter but does not actually go through with
+    application
+    :param session:
+    :param item:
+    :param zugang:
+    :return:
+    """
+    url_apply = (f"https://e-learning.tuhh.de/studip/dispatch.php/course/enrolment/apply/"
+                 f"{item['id']}")
     text = session.get(url_apply).text.splitlines()
-    if any(["nicht erfolgreich" in x for x in text]):
+    if any("nicht erfolgreich" in x for x in text):
         rule: str = ''
         for i, e in enumerate(text):
             if "messagebox_details" in e:
@@ -83,7 +94,7 @@ def attempt_apply(session: requests.Session, item: dict, zugang: dict) -> None:
             'name': item['name'],
             'rule': rule.strip()
         })
-    elif any(["wirklich anmelden" in x for x in text]):
+    elif any("wirklich anmelden" in x for x in text):
         zugang['erlaubt'] += 1
         # print("anmeldung erlaubt", item['name'])
     else:
